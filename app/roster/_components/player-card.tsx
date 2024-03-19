bc8774cc0564e5c595a4741abdb9a96b1f1e6a97
dwhddnjs <per1215@naver.com>
2024-03-20 00:23:10 +0900

    카드 컴포넌트 추가

---
+++ app/roster/_components/player-card.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import Image from "next/image"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { Medal } from "lucide-react"
+
+type PlayerTypes = {
+  id: number
+  img: string
+  nickname: string
+  name: string
+  career: string[] | null
+}
+
+interface PlayerCardProps {
+  player: PlayerTypes
+}
+
+export const PlayerCard = ({ player }: PlayerCardProps) => {
+  return (
+    <HoverCard>
+      <HoverCardTrigger>
+        <div className="bg-[#272727] rounded-md border-[1px] border-[#191919] hover:translate-y-2  duration-200 ease-linear shadow-md relative">
+          <div className="flex items-center justify-center">
+            <Image src={player.img} width={120} height={80} alt="" />
+          </div>
+          <div className="p-3 flex justify-between items-end ">
+            <div>
+              <h2 className="text-[#eeeeee] font-bold text-md">
+                {player.nickname}
+              </h2>
+              <p className="text-[#c4c4c4] text-xs">{player.name}</p>
+            </div>
+          </div>
+          <div className="absolute bottom-2 right-2">
+            <Image src="/images/top_icon_p.svg" width={24} height={24} alt="" />
+          </div>
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent className="bg-[#1a1a1a] border-0 relative drop-shadow-none">
+        <div className="w-0 h-0 border-l-[8px] border-l-transparent border-b-[12px] border-b-[#1a1a1a] border-r-[8px] border-r-transparent absolute top-[-12px] left-[45%]"></div>
+        <div className="flex justify-center items-center w-fit space-x-1 mb-2">
+          <Medal color="#fff" width={14} height={14} />
+          <h3 className="font-semibold text-white text-sm">주요 경력</h3>
+        </div>
+        <div className="space-y-0.5 ml-[4px]">
+          {!player?.career ? (
+            <p className="text-[#c4c4c4] text-xs">없음</p>
+          ) : (
+            player?.career?.map((career) => (
+              <p className="text-[#c4c4c4] text-xs" key={career}>
+                {`●  ${career}`}
+              </p>
+            ))
+          )}
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  )
+}
--- app/roster/page.tsx
+++ app/roster/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image"
 import { players } from "@/lib/player"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { PlayerCard } from "./_components/player-card"
 
 function RosterPage() {
   const { top } = players
@@ -20,27 +21,9 @@ function RosterPage() {
           <h3 className="text-4xl text-[#eeeeee] font-bold">Roster</h3>
           <p className="text-md text-[#c4c4c4]">당신의 로스터를 구성해보세요</p>
         </div>
-        <div className="grid grid-flow-row grid-cols-9 gap-3 ">
-          {top.map((el) => (
-            <div key={el.id} className="bg-[#272727] ">
-              <div className="">
-                <div className="">
-                  <Avatar
-                    key={el.id}
-                    className="w-[100px] h-[100px] bg-[#1a1a1a]"
-                  >
-                    <AvatarImage src={el.img} />
-                  </Avatar>
-                </div>
-                <div>
-                  <h2 className="text-[#eeeeee] font-bold text-md">
-                    {el.nickname}
-                  </h2>
-                  <p className="text-[#c4c4c4] text-xs">{el.name}</p>
-                  <p className="text-[#c4c4c4] text-xs">TOP</p>
-                </div>
-              </div>
-            </div>
+        <div className="grid grid-flow-row grid-cols-9 gap-4 ">
+          {top.map((player) => (
+            <PlayerCard player={player} key={player.id} />
           ))}
         </div>
       </div>

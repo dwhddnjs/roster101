ab39023824f50fcfbcee75ddb09ff45a6f954b63
dwhddnjs <per1215@naver.com>
2024-05-01 04:26:02 +0900

    모바일 캐러셀 수정

--- app/roster/[rosterId]/_components/profile-carousel.tsx
+++ app/roster/[rosterId]/_components/profile-carousel.tsx
@@ -30,67 +30,110 @@ export const ProfileCarousel = ({ setApi, players }: ProfileCarouselProps) => {
     <Carousel
       setApi={setApi}
       className={cn(
-        "w-[50.3%] h-full border-2 border-[#1e1e1e] drop-shadow-lg"
+        "w-[50.3%] h-full border-2 border-[#1e1e1e] drop-shadow-lg",
+        isMobile && "w-full"
       )}
     >
-      <CarouselPrevious />
-      <CarouselNext />
+      <CarouselPrevious className={cn("", isMobile && "w-7 h-7")} />
+      <CarouselNext className={cn("", isMobile && "w-7 h-7")} />
       <CarouselContent>
         {players?.map((player) => (
-          <CarouselItem
-            key={player.id}
-            className="flex w-full h-full bg-[#1e1e1e] relative"
-          >
-            <div className="flex bg-[#191919]">
-              <Image src={player.img} width={350} height={350} alt="" />
-            </div>
-            <div className="px-[23px] py-[12px] space-y-1 relative">
-              <h4 className="font-bold text-[48px] text-[#eeeeee]">
-                {player.nickname}
-              </h4>
-              <div className="flex  items-center space-x-2 ">
-                <User color="#c4c4c4" className="w-6 h-6 font-bold" />
-                <p className="text-[#c4c4c4] text-lg">{player.name}</p>
-              </div>
-              <div className="flex items-center space-x-[11px] pl-[3px]">
-                <Image
-                  src="/images/empty_esport_icon.svg"
-                  width={18}
-                  height={18}
-                  alt=""
-                />
-                <p className="text-[#c4c4c4] text-md ">
-                  {player.position.toUpperCase()}
-                </p>
-              </div>
-
-              <div className="space-y-1">
-                <div className="flex items-center space-x-2 ">
-                  <Trophy color="#c4c4c4" className="w-6 h-6 font-bold" />
-                  <p className="text-[#c4c4c4] text-md">경력</p>
+          <>
+            {isMobile ? (
+              <CarouselItem
+                key={player.id}
+                className="w-full h-[200px] bg-[#1e1e1e] flex"
+              >
+                <div className="flex bg-[#191919] w-3/7">
+                  <Image src={player.img} width={150} height={300} alt="" />
                 </div>
-                <div className="pl-[32px] max-h-[240px] overflow-y-auto min-w-[300px]">
-                  {player.career.map((career) => (
-                    <p className="text-[#555555] text-sm" key={career}>
-                      {`●  ${career}`}
+                <div className="w-1/2 px-3 py-2">
+                  <h4 className="font-bold text-md text-[#eeeeee]">
+                    {player.nickname}
+                  </h4>
+                  <div className="flex items-center space-x-1 ">
+                    <User color="#c4c4c4" className="w-3 h-3 font-bold" />
+                    <p className="text-[#c4c4c4] text-xs">{player.name}</p>
+                  </div>
+                  <div className="flex items-center space-x-1">
+                    <Image
+                      src="/images/empty_esport_icon.svg"
+                      width={12}
+                      height={12}
+                      alt=""
+                    />
+                    <p className="text-[#c4c4c4] text-xs ">
+                      {player.position.toUpperCase()}
                     </p>
-                  ))}
+                  </div>
+                  <div className="space-y-1">
+                    <div className="flex items-center space-x-1 ">
+                      <Trophy color="#c4c4c4" className="w-3 h-3 font-bold" />
+                      <p className="text-[#c4c4c4] text-xs">경력</p>
+                    </div>
+                    <div className="max-h-[100px] overflow-y-auto min-w-[160px] ">
+                      {player.career.map((career) => (
+                        <p className="text-[#555555] text-xs" key={career}>
+                          {`● ${career}`}
+                        </p>
+                      ))}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="absolute bottom-5 right-5">
-              <Image
-                src={renderPositionImg(player.position)}
-                alt=""
-                height={48}
-                width={48}
-              />
-            </div>
-          </CarouselItem>
-          // <CarouselItem
-          //   key={player.id}
-          //   className="w-full h-[200px] border-2"
-          // ></CarouselItem>
+              </CarouselItem>
+            ) : (
+              <CarouselItem
+                key={player.id}
+                className="flex w-full h-full bg-[#1e1e1e] relative"
+              >
+                <div className="flex bg-[#191919] ">
+                  <Image src={player.img} width={355} height={100} alt="" />
+                </div>
+                <div className="px-[23px] py-[12px] space-y-1 relative">
+                  <h4 className="font-bold text-[48px] text-[#eeeeee]">
+                    {player.nickname}
+                  </h4>
+                  <div className="flex  items-center space-x-2 ">
+                    <User color="#c4c4c4" className="w-6 h-6 font-bold" />
+                    <p className="text-[#c4c4c4] text-lg">{player.name}</p>
+                  </div>
+                  <div className="flex items-center space-x-[11px] pl-[3px]">
+                    <Image
+                      src="/images/empty_esport_icon.svg"
+                      width={18}
+                      height={18}
+                      alt=""
+                    />
+                    <p className="text-[#c4c4c4] text-md ">
+                      {player.position.toUpperCase()}
+                    </p>
+                  </div>
+
+                  <div className="space-y-1">
+                    <div className="flex items-center space-x-2 ">
+                      <Trophy color="#c4c4c4" className="w-6 h-6 font-bold" />
+                      <p className="text-[#c4c4c4] text-md">경력</p>
+                    </div>
+                    <div className="pl-[32px] max-h-[240px] overflow-y-auto min-w-[300px]">
+                      {player.career.map((career) => (
+                        <p className="text-[#555555] text-sm" key={career}>
+                          {`●  ${career}`}
+                        </p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute bottom-5 right-5">
+                  <Image
+                    src={renderPositionImg(player.position)}
+                    alt=""
+                    height={48}
+                    width={48}
+                  />
+                </div>
+              </CarouselItem>
+            )}
+          </>
         ))}
       </CarouselContent>
     </Carousel>
